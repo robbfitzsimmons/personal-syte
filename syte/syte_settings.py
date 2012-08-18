1b4304c21ad8e0a4f8cb4c17ba760debcb6cd961
Robb Fitzsimmons <robb@proximate.com>
2012-08-18 02:54:22 -0400

    Added required heroku settings

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-DEPLOYMENT_MODE = 'dev'
+DEPLOYMENT_MODE = 'prod'
 COMPRESS_REVISION_NUMBER = '1.0'
 
 
@@ -102,6 +102,6 @@ if DEPLOYMENT_MODE == 'dev':
     DEBUG = True
 else:
     DEBUG = False
-    SITE_ROOT_URI = '[ENTER PROD URL HERE] ex. http://rigoneri.herokuapp.com/'
+    SITE_ROOT_URI = 'http://warm-beach-4150.herokuapp.com/'
 
 MEDIA_URL = SITE_ROOT_URI + 'static/'
